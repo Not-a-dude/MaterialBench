c21a05a07cb305e7b1eae7e7255ce13902445709
Not-a-dude <honkhonkovic842@gmail.com>
2026-03-03 16:25:57 +0700

    build: use http logging interceptor

--- app/src/main/java/com/komarudude/materialbench/utils/ApiService.kt
+++ app/src/main/java/com/komarudude/materialbench/utils/ApiService.kt
@@ -1,9 +1,14 @@
 package com.komarudude.materialbench.utils
 
+import com.komarudude.materialbench.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 data class ScoreRequest(
     val score: Int,
@@ -29,9 +34,25 @@ interface ApiService {
 object RetrofitClient {
     private const val BASE_URL = "https://dude.privetbradok.ru:9560/"
 
+    private val okHttpClient: OkHttpClient by lazy {
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+            builder.addInterceptor(logging)
+        }
+
+        builder.build()
+    }
+
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
