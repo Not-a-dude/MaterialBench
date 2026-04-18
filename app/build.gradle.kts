b4838c248243ae2bcb2d8398c347990d7db9ae13
Not-a-dude <honkhonkovic842@gmail.com>
2026-04-18 16:55:38 +0700

    build: bump versionCode, versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.komarudude.materialbench"
         minSdk = 30
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.4.0"
+        versionCode = 24
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
