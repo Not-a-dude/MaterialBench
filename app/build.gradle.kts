c9430b3812366b07eb2b54dfd88f83dd3ee71f45
Not-a-dude <honkhonkovic842@gmail.com>
2026-04-19 20:02:16 +0700

    chore: add MaterialBench assets with LFS support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.komarudude.materialbench"
         minSdk = 30
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.4.1"
+        versionCode = 25
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
